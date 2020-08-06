478e98dadc90ffe3958c860c3ba0eb8d439bf283
abbey <abbey.brem@gmail.com>
2020-08-06 15:39:29 +1200

    Adds mind maps from wednesday 6th meeting, about question development

---
+++ Figures/MindMaps/ProjectMap.mm
@@ -0,0 +1,110 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Question" FOLDED="false" ID="ID_1988881031" CREATED="1596680226151" MODIFIED="1596680236590" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" zoom="1.331">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="5" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Why?" POSITION="right" ID="ID_193752305" CREATED="1596680237222" MODIFIED="1596680240034">
+<edge COLOR="#ff0000"/>
+<node TEXT="LiteratureReview" ID="ID_471761801" CREATED="1596680276413" MODIFIED="1596680281310"/>
+<node TEXT="SMART" ID="ID_290990455" CREATED="1596680281657" MODIFIED="1596680284725">
+<node TEXT="Specific" ID="ID_301230906" CREATED="1596680297081" MODIFIED="1596680299982"/>
+<node TEXT="Measurable" ID="ID_846226792" CREATED="1596680300620" MODIFIED="1596680309091"/>
+<node TEXT="Achievable" ID="ID_1687799921" CREATED="1596680310199" MODIFIED="1596680312342"/>
+<node TEXT="Relevant" ID="ID_1336151521" CREATED="1596680312496" MODIFIED="1596680316399"/>
+<node TEXT="TimeBound" ID="ID_714624129" CREATED="1596680316830" MODIFIED="1596680320041"/>
+</node>
+<node TEXT="Benefits" ID="ID_1348274139" CREATED="1596680285032" MODIFIED="1596680290058"/>
+</node>
+<node TEXT="What?" POSITION="right" ID="ID_1021872700" CREATED="1596680240401" MODIFIED="1596680243074">
+<edge COLOR="#0000ff"/>
+<node TEXT="Report" ID="ID_1722705976" CREATED="1596680323842" MODIFIED="1596680554702">
+<node TEXT="Background and Data" ID="ID_346492742" CREATED="1596680515531" MODIFIED="1596680520052"/>
+<node TEXT="Ethics, Privacy and security" ID="ID_810890101" CREATED="1596680520392" MODIFIED="1596680636971"/>
+<node TEXT="Exploratory data analysis" ID="ID_841080390" CREATED="1596680533303" MODIFIED="1596680540869"/>
+<node TEXT="Individual contributions" ID="ID_1074071760" CREATED="1596680541164" MODIFIED="1596680550100"/>
+</node>
+</node>
+<node TEXT="Who" POSITION="right" ID="ID_1128211065" CREATED="1596680243411" MODIFIED="1596680247360">
+<edge COLOR="#00ff00"/>
+<node TEXT="Supervisor" ID="ID_1039766339" CREATED="1596680333039" MODIFIED="1596680342453">
+<node TEXT="Aljehandro" ID="ID_204942260" CREATED="1596680385299" MODIFIED="1596680391450"/>
+</node>
+<node TEXT="Team" ID="ID_1362670007" CREATED="1596680342863" MODIFIED="1596680345003"/>
+</node>
+<node TEXT="When and What with?" POSITION="right" ID="ID_1182876351" CREATED="1596680247715" MODIFIED="1596680257166">
+<edge COLOR="#ff00ff"/>
+<node TEXT="Resources" ID="ID_214655169" CREATED="1596680354934" MODIFIED="1596680357766">
+<node TEXT="R - CRAN gis formats and maps" ID="ID_903415687" CREATED="1596680582039" MODIFIED="1596680627507"/>
+<node TEXT="Data" ID="ID_1433734245" CREATED="1596680801201" MODIFIED="1596680805654"/>
+</node>
+<node TEXT="Deliverables" ID="ID_1267709786" CREATED="1596680358339" MODIFIED="1596680362133">
+<node TEXT="Phase 1 due: 31st August" ID="ID_1290716620" CREATED="1596680462764" MODIFIED="1596680487338"/>
+</node>
+<node TEXT="Restrictions" ID="ID_510911111" CREATED="1596680362448" MODIFIED="1596680370998"/>
+</node>
+<node TEXT="How?" POSITION="right" ID="ID_582485592" CREATED="1596680257417" MODIFIED="1596680376639">
+<edge COLOR="#00ffff"/>
+</node>
+</node>
+</map>
---
+++ Figures/MindMaps/QuestionDevelopment.mm
@@ -0,0 +1,117 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Patterns" FOLDED="false" ID="ID_346595010" CREATED="1596679713509" MODIFIED="1596683412873" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" zoom="1.331">
+    <properties edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff" fit_to_viewport="false"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="9" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Dangerous Neighbourhoods" POSITION="right" ID="ID_1569418113" CREATED="1596679962594" MODIFIED="1596682366892" HGAP_QUANTITY="56.74999872595075 pt" VSHIFT_QUANTITY="-34.499998971819906 pt">
+<edge COLOR="#ff0000"/>
+<node TEXT="Difficult ethically to categorise" ID="ID_879584036" CREATED="1596681928853" MODIFIED="1596681965161"/>
+<node TEXT="Possibly done before" ID="ID_1982010972" CREATED="1596681966307" MODIFIED="1596681974586"/>
+<node TEXT="Heard about it before" ID="ID_1633621279" CREATED="1596682030071" MODIFIED="1596682036381"/>
+<node TEXT="Alot of data around this." ID="ID_248201830" CREATED="1596682261796" MODIFIED="1596682455111"/>
+<node TEXT="Comes up alot in the nz police website" ID="ID_1501060776" CREATED="1596682082031" MODIFIED="1596682134626"/>
+</node>
+<node TEXT="crimes with the size of the community" POSITION="right" ID="ID_147965774" CREATED="1596682143538" MODIFIED="1596682522034" HGAP_QUANTITY="52.249998860061204 pt" VSHIFT_QUANTITY="9.749999709427366 pt">
+<edge COLOR="#00007c"/>
+<node TEXT="Quite a few reports about population size" ID="ID_1156286119" CREATED="1596682221837" MODIFIED="1596682522034" HGAP_QUANTITY="16.999999910593036 pt" VSHIFT_QUANTITY="-15.749999530613437 pt"/>
+<node TEXT="How acurate can we get the community size" ID="ID_1542481136" CREATED="1596683011929" MODIFIED="1596683022816"/>
+</node>
+<node TEXT="crime and type of area (commercial, residential ect)" POSITION="right" ID="ID_1891947716" CREATED="1596680076640" MODIFIED="1596682363493" HGAP_QUANTITY="46.24999903887513 pt" VSHIFT_QUANTITY="25.4999992400408 pt">
+<edge COLOR="#ff00ff"/>
+<node TEXT="Could be interesting with the covid" ID="ID_1351000335" CREATED="1596682529419" MODIFIED="1596682567379"/>
+<node TEXT="Something a bit different to just regional breakdown" ID="ID_1928924476" CREATED="1596682567579" MODIFIED="1596682581230"/>
+<node TEXT="Could focus in on a region and then look at the area type within this e.g Auckland or Wellington" ID="ID_1892242519" CREATED="1596682601856" MODIFIED="1596682666434"/>
+<node TEXT="Would need enough variation to maintain data quality" ID="ID_1845634253" CREATED="1596682620698" MODIFIED="1596682697581"/>
+</node>
+<node TEXT="crime and age demographic of the community" POSITION="right" ID="ID_724190731" CREATED="1596680092356" MODIFIED="1596682361509" HGAP_QUANTITY="48.499998971819906 pt" VSHIFT_QUANTITY="23.249999307096026 pt">
+<edge COLOR="#00ffff"/>
+<node TEXT="Is there alot of information about this?" ID="ID_167561536" CREATED="1596682586807" MODIFIED="1596682706099"/>
+<node TEXT="Would be intesting paired with somethin like size of community or type of area." ID="ID_3347619" CREATED="1596682706463" MODIFIED="1596682796407">
+<node TEXT="" ID="ID_120228026" CREATED="1596682764925" MODIFIED="1596682764925"/>
+<node TEXT="Is the demographic linked to the types of crime" ID="ID_470953177" CREATED="1596682796409" MODIFIED="1596682813285"/>
+</node>
+<node TEXT="Age could be difficult to find" ID="ID_454647566" CREATED="1596682751710" MODIFIED="1596682871004">
+<node TEXT="Census? voting?" ID="ID_318793437" CREATED="1596682871005" MODIFIED="1596682911297"/>
+<node TEXT="Age is listed in some of the crime datasets." ID="ID_1003541115" CREATED="1596682911622" MODIFIED="1596682937381"/>
+<node TEXT="The age demographic of the community may not be the same as the demographic of the criminals in the area." ID="ID_1032212567" CREATED="1596682937577" MODIFIED="1596682964869"/>
+</node>
+<node TEXT="Would we only have good info on people over 18?" ID="ID_680379513" CREATED="1596682965770" MODIFIED="1596683001689"/>
+</node>
+<node TEXT="crimes with distance to public services" POSITION="right" ID="ID_1966045978" CREATED="1596680036745" MODIFIED="1596682285550" HGAP_QUANTITY="39.4999992400408 pt" VSHIFT_QUANTITY="16.499999508261695 pt">
+<edge COLOR="#00ff00"/>
+<node TEXT="Interesting with community size" ID="ID_924074809" CREATED="1596683025137" MODIFIED="1596683143214"/>
+<node TEXT="Narrow it down with a type of crime and a public service." ID="ID_1487100667" CREATED="1596683042889" MODIFIED="1596683177974">
+<node TEXT="Need specificity to draw good conclusions" ID="ID_697475512" CREATED="1596683105868" MODIFIED="1596683122575"/>
+</node>
+<node TEXT="Need to have a look at whats avalible." ID="ID_1872943647" CREATED="1596683126898" MODIFIED="1596683219396"/>
+</node>
+<node TEXT="Covid-19 pandemic and crime rates" POSITION="right" ID="ID_1856624143" CREATED="1596682270931" MODIFIED="1596682287818" HGAP_QUANTITY="28.24999957531692 pt" VSHIFT_QUANTITY="17.99999946355821 pt">
+<edge COLOR="#007c00"/>
+<node TEXT="Some newer information" ID="ID_778070812" CREATED="1596682294555" MODIFIED="1596682321879"/>
+<node TEXT="Crime type changes?" ID="ID_1464102669" CREATED="1596682322151" MODIFIED="1596682341390"/>
+<node TEXT="Short time periods e.g level 4 was only 4 weeks" ID="ID_694929984" CREATED="1596682341709" MODIFIED="1596682354766"/>
+<node TEXT="" ID="ID_420434213" CREATED="1596682355044" MODIFIED="1596682355044"/>
+</node>
+</node>
+</map>
