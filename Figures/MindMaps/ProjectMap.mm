b6c194e67701da8ecd5013fe40a4f6dae2a74ca9
abbey <abbey.brem@gmail.com>
2020-08-11 09:48:22 +1200

    Updates project mindmap based on updated question - unfinished

--- Figures/MindMaps/ProjectMap.mm
+++ Figures/MindMaps/ProjectMap.mm
@@ -1,8 +1,8 @@
 <map version="freeplane 1.8.0">
 <!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
-<node TEXT="Crime and type of area" FOLDED="false" ID="ID_1988881031" CREATED="1596680226151" MODIFIED="1596947915970" STYLE="oval">
+<node TEXT="Investigate the pattern between population, house price and crime." FOLDED="false" ID="ID_1988881031" CREATED="1596680226151" MODIFIED="1597030824352" STYLE="oval">
 <font SIZE="18"/>
-<hook NAME="MapStyle" zoom="1.771">
+<hook NAME="MapStyle" zoom="1.21">
     <properties fit_to_viewport="false" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff"/>
 
 <map_styles>
@@ -63,10 +63,13 @@
 </stylenode>
 </map_styles>
 </hook>
-<hook NAME="AutomaticEdgeColor" COUNTER="6" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="AutomaticEdgeColor" COUNTER="10" RULE="ON_BRANCH_CREATION"/>
 <node TEXT="Why?" POSITION="right" ID="ID_193752305" CREATED="1596680237222" MODIFIED="1596680240034">
 <edge COLOR="#ff0000"/>
-<node TEXT="LiteratureReview" ID="ID_471761801" CREATED="1596680276413" MODIFIED="1596680281310"/>
+<node TEXT="LiteratureReview" ID="ID_471761801" CREATED="1596680276413" MODIFIED="1596680281310">
+<node TEXT="Change in NZ crime stats 2014" ID="ID_694760654" CREATED="1597030269028" MODIFIED="1597030285434"/>
+<node TEXT="Effect of crime on house price." ID="ID_1909659937" CREATED="1597030544024" MODIFIED="1597030573040"/>
+</node>
 <node TEXT="SMART" ID="ID_290990455" CREATED="1596680281657" MODIFIED="1596680284725">
 <node TEXT="Specific" ID="ID_301230906" CREATED="1596680297081" MODIFIED="1596680299982"/>
 <node TEXT="Measurable" ID="ID_846226792" CREATED="1596680300620" MODIFIED="1596680309091"/>
@@ -79,7 +82,21 @@
 <node TEXT="What?" POSITION="right" ID="ID_1021872700" CREATED="1596680240401" MODIFIED="1596680243074">
 <edge COLOR="#0000ff"/>
 <node TEXT="Report" ID="ID_1722705976" CREATED="1596680323842" MODIFIED="1596680554702">
-<node TEXT="Background and Data" ID="ID_346492742" CREATED="1596680515531" MODIFIED="1596680520052"/>
+<node TEXT="Background and Data" ID="ID_346492742" CREATED="1596680515531" MODIFIED="1596680520052">
+<node TEXT="Data" ID="ID_820715409" CREATED="1597031103566" MODIFIED="1597031105983">
+<node TEXT="Victimisations by Time and Place" FOLDED="true" ID="ID_69166532" CREATED="1597031105985" MODIFIED="1597031114632">
+<node TEXT="Type of crime (ANSOC divison/Group/subdivision)" ID="ID_1716491061" CREATED="1597031114634" MODIFIED="1597031221606"/>
+<node TEXT="Location (suburb) and the location type" ID="ID_1364212370" CREATED="1597031117969" MODIFIED="1597031163027"/>
+<node TEXT="Meshblock number" ID="ID_1297154105" CREATED="1597031163574" MODIFIED="1597031370274"/>
+<node TEXT="Day of the week + month/year" ID="ID_20060151" CREATED="1597031370865" MODIFIED="1597031427365"/>
+<node TEXT="Number of reports and victimisations" ID="ID_220440129" CREATED="1597031301809" MODIFIED="1597031439723"/>
+</node>
+<node TEXT="House prices" ID="ID_1117134555" CREATED="1597031441962" MODIFIED="1597031450854"/>
+<node TEXT="2013 census data" ID="ID_494097031" CREATED="1597031451307" MODIFIED="1597031894637">
+<node TEXT="Population by meshblock and by area unit" ID="ID_1947225887" CREATED="1597031894639" MODIFIED="1597031910204"/>
+</node>
+</node>
+</node>
 <node TEXT="Ethics, Privacy and security" ID="ID_810890101" CREATED="1596680520392" MODIFIED="1596680636971"/>
 <node TEXT="Exploratory data analysis" ID="ID_841080390" CREATED="1596680533303" MODIFIED="1596680540869"/>
 <node TEXT="Individual contributions" ID="ID_1074071760" CREATED="1596680541164" MODIFIED="1596680550100"/>
@@ -108,6 +125,18 @@
 </node>
 <node TEXT="Sub Pattern of Wealth of area" POSITION="left" ID="ID_917630945" CREATED="1596948008857" MODIFIED="1596948043843">
 <edge COLOR="#7c0000"/>
+</node>
+<node TEXT="House price in the area" POSITION="left" ID="ID_1460399009" CREATED="1597030161478" MODIFIED="1597030168129">
+<edge COLOR="#00007c"/>
+</node>
+<node TEXT="Possible question: Does the average house price have an effect on the types of crime in that area?" POSITION="left" ID="ID_1702647573" CREATED="1597030398194" MODIFIED="1597030438770">
+<edge COLOR="#7c007c"/>
+</node>
+<node TEXT="Possible question: Does population density have a pattern with  the type of crime and house price?" POSITION="left" ID="ID_1220886775" CREATED="1597030573988" MODIFIED="1597030732897">
+<edge COLOR="#007c7c"/>
+</node>
+<node TEXT="Pattern of wealth of community and type of crime" POSITION="left" ID="ID_363771601" CREATED="1597030362645" MODIFIED="1597030389842">
+<edge COLOR="#007c00"/>
 </node>
 </node>
 </map>
