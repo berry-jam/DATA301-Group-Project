3ce730b157b911f337148a06ce04ae095b6227e0
Abbey <abbey.bremner@outlook.com>
2020-08-25 16:40:59 +1200

    Updates mindmap based on change of question

---
+++ Figures/MindMaps/ProjectMapV3(assault).mm
@@ -0,0 +1,189 @@
+<map version="freeplane 1.8.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Investigate whether covid-19 has had an effect on assault in New Zealand." FOLDED="false" ID="ID_1988881031" CREATED="1596680226151" MODIFIED="1598330201514" STYLE="oval">
+<font SIZE="18"/>
+<hook NAME="MapStyle" zoom="1.241">
+    <properties fit_to_viewport="false" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10"/>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11"/>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="13" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="Why?" POSITION="right" ID="ID_193752305" CREATED="1596680237222" MODIFIED="1596680240034">
+<edge COLOR="#ff0000"/>
+<node TEXT="LiteratureReview" FOLDED="true" ID="ID_471761801" CREATED="1596680276413" MODIFIED="1596680281310">
+<node TEXT="News articles reporting on change in crime stats (specifically domestic violence)" ID="ID_1684638410" CREATED="1597123259868" MODIFIED="1597123384770"/>
+<node TEXT="Reports for more foreign countries than NZ" ID="ID_1067684966" CREATED="1597123440232" MODIFIED="1597123453516"/>
+<node TEXT="Typically crime rates are down but domestic violence is up." ID="ID_1828277770" CREATED="1597123454019" MODIFIED="1597123472228"/>
+</node>
+<node TEXT="SMART" ID="ID_290990455" CREATED="1596680281657" MODIFIED="1596680284725">
+<node TEXT="Specific" ID="ID_301230906" CREATED="1596680297081" MODIFIED="1596680299982">
+<node TEXT="Looking at Assault" ID="ID_1402340785" CREATED="1597123802860" MODIFIED="1598330239774">
+<font STRIKETHROUGH="false"/>
+<node TEXT="Domestic violence is a subcategory of Assault in the ANSOC classifications" ID="ID_1482928448" CREATED="1598330239779" MODIFIED="1598330261122"/>
+</node>
+<node TEXT="(Possible) subset patterns of location and COVID-19 lockdown level" ID="ID_1013606971" CREATED="1597123815985" MODIFIED="1597123843642"/>
+</node>
+<node TEXT="Measurable" ID="ID_846226792" CREATED="1596680300620" MODIFIED="1596680309091">
+<node TEXT="% change in domestic violence reports/victimisations" ID="ID_1255098079" CREATED="1597123852962" MODIFIED="1597123896621"/>
+<node TEXT="Comparison of time period" FOLDED="true" ID="ID_857553463" CREATED="1597123897161" MODIFIED="1597123912302">
+<node TEXT="More frequent reports" ID="ID_645383236" CREATED="1597123912304" MODIFIED="1597123933072"/>
+<node TEXT="Comparing covid months with the same time frames in previous years" ID="ID_875688830" CREATED="1598330275558" MODIFIED="1598330294861"/>
+</node>
+<node TEXT="Report is measured through deadlines and trello progression" ID="ID_614859357" CREATED="1598330319798" MODIFIED="1598330341416"/>
+</node>
+<node TEXT="Achievable" ID="ID_1687799921" CREATED="1596680310199" MODIFIED="1596680312342">
+<node TEXT="Has been done before in other countries" ID="ID_1946581316" CREATED="1597123934879" MODIFIED="1597123942406"/>
+<node TEXT="Data is available and ongoing" ID="ID_1821371996" CREATED="1597123942764" MODIFIED="1598330310356"/>
+</node>
+<node TEXT="Relevant" ID="ID_1336151521" CREATED="1596680312496" MODIFIED="1596680316399">
+<node TEXT="Still happening" ID="ID_1011341428" CREATED="1597123991400" MODIFIED="1597124001727"/>
+<node TEXT="Current situation for which strategies are still be devised (prevention)" ID="ID_1347714188" CREATED="1597124002088" MODIFIED="1597124034460"/>
+</node>
+<node TEXT="TimeBound" ID="ID_714624129" CREATED="1596680316830" MODIFIED="1596680320041">
+<node TEXT="Due date for report: 31st August" FOLDED="true" ID="ID_1303668754" CREATED="1597123750992" MODIFIED="1597123764655">
+<node TEXT="" ID="ID_141298995" CREATED="1597123764657" MODIFIED="1597123764657"/>
+</node>
+<node TEXT="We are only comparing COVID time (2020) and 1 other year (2019)" ID="ID_1314009676" CREATED="1597123768097" MODIFIED="1597123788429"/>
+</node>
+</node>
+<node TEXT="Benefits" ID="ID_1348274139" CREATED="1596680285032" MODIFIED="1596680290058">
+<node TEXT="Give insight into the effects lockdown has had on domestic violence and other kinds of assault" ID="ID_1507434850" CREATED="1597124044104" MODIFIED="1598330357267"/>
+<node TEXT="Info gathered from location data could also help to direct prevention/help." ID="ID_1161734599" CREATED="1597124060532" MODIFIED="1597124083094"/>
+<node TEXT="Prediction of future trends if we were to go back into more severe lockdown" ID="ID_475289543" CREATED="1597124083531" MODIFIED="1597124106311"/>
+</node>
+</node>
+<node TEXT="What?" POSITION="right" ID="ID_1021872700" CREATED="1596680240401" MODIFIED="1596680243074">
+<edge COLOR="#0000ff"/>
+<node TEXT="Report" ID="ID_1722705976" CREATED="1596680323842" MODIFIED="1596680554702">
+<node TEXT="Background and Data" ID="ID_346492742" CREATED="1596680515531" MODIFIED="1596680520052">
+<node TEXT="Patterns have been identified in other countries + NZ" ID="ID_1192015286" CREATED="1597124250183" MODIFIED="1597124531634">
+<node TEXT="Brazil hads a large increase in domestic violence and homicide" ID="ID_20660648" CREATED="1597124469237" MODIFIED="1597124486216"/>
+<node TEXT="USA also had an increase in the same" ID="ID_1104781844" CREATED="1597124489383" MODIFIED="1597124507986"/>
+</node>
+<node TEXT="There is covid data on breaches gathered by the COVID-19 hotline" ID="ID_932763747" CREATED="1597124408891" MODIFIED="1597124422040"/>
+<node TEXT="Also data gathered on types of crime" ID="ID_858426685" CREATED="1597124422324" MODIFIED="1597124437202"/>
+</node>
+<node TEXT="Ethics, Privacy and security" ID="ID_810890101" CREATED="1596680520392" MODIFIED="1596680636971">
+<node TEXT="Assault is a sensitive topic sso data is already clean as it is publicised by police.nz" FOLDED="true" ID="ID_946363275" CREATED="1597124463430" MODIFIED="1598330387133">
+<node TEXT="Information released is already anonymised" ID="ID_1136357854" CREATED="1597124555897" MODIFIED="1597124778762"/>
+</node>
+<node TEXT="Ethically publication of hotspots/ spots of increase may be a concern depending on how small we go." FOLDED="true" ID="ID_1508691597" CREATED="1597124620055" MODIFIED="1597124673473">
+<node TEXT="Region would be fine, but suburb/district may need to be addressed with concern to privacy/ethics" ID="ID_1775012704" CREATED="1597124673475" MODIFIED="1597124699604"/>
+</node>
+<node TEXT="Generally quite a discussed topic, not so controversial to be documented" ID="ID_830732029" CREATED="1597124713390" MODIFIED="1597124763787"/>
+<node TEXT="We arent identifying covid cases which is a major concern currently" ID="ID_1519579234" CREATED="1597124735161" MODIFIED="1597124801860"/>
+<node TEXT="We are using anonymous crime stats data" ID="ID_81035428" CREATED="1597124802337" MODIFIED="1597124843178">
+<node TEXT="No personal data is published" ID="ID_1937856311" CREATED="1597124843180" MODIFIED="1597124853186"/>
+</node>
+<node TEXT="Speculating on causes will require formal research" ID="ID_1784477811" CREATED="1597125365906" MODIFIED="1597125410390">
+<node TEXT="needs to be done carefully and respectfully" ID="ID_825536736" CREATED="1597125411742" MODIFIED="1597125426235"/>
+</node>
+</node>
+<node TEXT="Exploratory data analysis" ID="ID_841080390" CREATED="1596680533303" MODIFIED="1596680540869">
+<node TEXT="Work out the percentage of domestic violence crimes/reports" ID="ID_1712512429" CREATED="1597124862609" MODIFIED="1597124918818"/>
+<node TEXT="Basic exploratory plots to investigate associations" ID="ID_1866663042" CREATED="1597124926742" MODIFIED="1597124941045"/>
+<node TEXT="data type analysis" ID="ID_198412804" CREATED="1598330401408" MODIFIED="1598330412370"/>
+<node TEXT="Spatial distributions and comparisons" ID="ID_1348332930" CREATED="1597124942107" MODIFIED="1597125051396"/>
+</node>
+<node TEXT="Individual contributions" FOLDED="true" ID="ID_1074071760" CREATED="1596680541164" MODIFIED="1596680550100">
+<node TEXT="tbc - report" ID="ID_1926844835" CREATED="1597125053342" MODIFIED="1597125063322"/>
+</node>
+</node>
+</node>
+<node TEXT="Who" POSITION="right" ID="ID_1128211065" CREATED="1596680243411" MODIFIED="1596680247360">
+<edge COLOR="#00ff00"/>
+<node TEXT="Supervisor" ID="ID_1039766339" CREATED="1596680333039" MODIFIED="1596680342453">
+<node TEXT="Aljehandro" ID="ID_204942260" CREATED="1596680385299" MODIFIED="1596680391450"/>
+</node>
+<node TEXT="Team" ID="ID_1362670007" CREATED="1596680342863" MODIFIED="1596680345003">
+<node TEXT="in trello" ID="ID_1746228360" CREATED="1597125067894" MODIFIED="1597125076369"/>
+</node>
+</node>
+<node TEXT="When and What with?" POSITION="right" ID="ID_1182876351" CREATED="1596680247715" MODIFIED="1596680257166">
+<edge COLOR="#ff00ff"/>
+<node TEXT="Resources" ID="ID_214655169" CREATED="1596680354934" MODIFIED="1597125128552"/>
+<node TEXT="Tools" ID="ID_1343820486" CREATED="1597125128901" MODIFIED="1597125131062">
+<node TEXT="R" ID="ID_1894687972" CREATED="1597125131064" MODIFIED="1597125132439">
+<node TEXT="GIS packages" ID="ID_1761882926" CREATED="1597125149997" MODIFIED="1597125188538"/>
+</node>
+<node TEXT="R markdown" ID="ID_958890123" CREATED="1597125132784" MODIFIED="1597125136728"/>
+</node>
+<node TEXT="Deliverables" ID="ID_1267709786" CREATED="1596680358339" MODIFIED="1596680362133">
+<node TEXT="Phase 1 due: 31st August" ID="ID_1290716620" CREATED="1596680462764" MODIFIED="1596680487338"/>
+</node>
+<node TEXT="Restrictions" ID="ID_510911111" CREATED="1596680362448" MODIFIED="1596680370998">
+<node TEXT="data is new and minimal" ID="ID_676743350" CREATED="1597125203526" MODIFIED="1597125232389">
+<node TEXT="Short time period" ID="ID_1706252382" CREATED="1597125255319" MODIFIED="1597125277155"/>
+</node>
+<node TEXT="research is emerging" ID="ID_1455018790" CREATED="1597125232723" MODIFIED="1597125242493"/>
+</node>
+</node>
+<node TEXT="How?" POSITION="right" ID="ID_582485592" CREATED="1596680257417" MODIFIED="1596680376639">
+<edge COLOR="#00ffff"/>
+<node TEXT="Trello Team has tasks assigned to members." ID="ID_293527039" CREATED="1597125280226" MODIFIED="1597125365188"/>
+<node TEXT="Cost" ID="ID_1332848512" CREATED="1597125463663" MODIFIED="1597125466067">
+<node TEXT="NA" ID="ID_1374778720" CREATED="1597125488017" MODIFIED="1597125493491"/>
+</node>
+<node TEXT="Risk" ID="ID_1574869672" CREATED="1597125466418" MODIFIED="1597125469007">
+<node TEXT="inadequate quantity of data" ID="ID_1369239235" CREATED="1597125469009" MODIFIED="1597125479707"/>
+<node TEXT="hard to find data" ID="ID_536435080" CREATED="1597125479968" MODIFIED="1597125485741"/>
+</node>
+</node>
+</node>
+</map>
